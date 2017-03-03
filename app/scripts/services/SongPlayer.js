f3aa35b44d168cc0e8fa1f6bd67532ed2146296b
Lindsay Koprivica <lindsayeplin@gmail.com>
2017-03-03 10:16:20 -0700

    finished checkpoint 7

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,14 +1,59 @@
 (function() {
      function SongPlayer() {
-          var SongPlayer = {};
+        var SongPlayer = {};
+           
+        var currentSong = null;
+        /**
+        * @desc Buzz object audio file
+        * @type {Object}
+        */
+        var currentBuzzObject = null;
+         /**
+         * @function setSong
+         * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+         * @param {Object} song
+         */
+        var setSong = function(song) {
+          if (currentBuzzObject) {
+              currentBuzzObject.stop();
+              currentSong.playing = null;
+    }
+ 
+        currentBuzzObject = new buzz.sound(song.audioUrl, {
+            formats: ['mp3'],
+            preload: true
+    });
+ 
+    currentSong = song;
+ };
        
         SongPlayer.play = function(song) {
-         var currentBuzzObject = new buzz.sound(song.audioUrl, {
-             formats: ['mp3'],
-             preload: true
+          if (currentSong !== song) {
+            
+              setSong(song);
+              currentBuzzObject.play(); 
+              song.playing = true;
+          
+             
+          } else if (currentSong === song) {
+              if (currentBuzzObject.isPaused()) {
+                 currentBuzzObject.play();
+         }
+     }    
+          
+  };
+          
+         SongPlayer.pause = function(song) {
+                 currentBuzzObject.pause();
+                 song.playing = false;
+ };
+            var currentBuzzObject = new buzz.sound(song.audioUrl, {
+                formats: ['mp3'],
+                preload: true
          });
- 
-         currentBuzzObject.play();    
+            
+         
+       }
      };
 
           return SongPlayer;
